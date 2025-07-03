da21af95fa5fc774430808c7020124e86874a934
Will Sulzer <willsulzer@google.com>
2025-07-03 00:51:02 +0000

    create scheduled jobs to perform the backups for both clusters

--- setup-cloud-run-scheduled-job.sh
+++ setup-cloud-run-scheduled-job.sh
@@ -1,55 +1,40 @@
 #!/bin/bash
 set -eux
-CLOUD_RUN_JOB_NAME="bfg-backup-job"
-CLOUD_SCHEDULER_JOB_NAME="bfg-backup-scheduler"
 
-export PROJECT_ID=will-gke-multi-region-bfg
-export SOURCE_CLUSTER=bfg-cluster-us-central1
-export REGION=us-central1
-export DR_REGION=us-west1
-export NAMESPACE=bfg
-export BACKUP_NAME=bkp-bfg-bkp-plan
-export BACKUP_PLAN_NAME=bfg-bkp-plan
-export PV_STORAGE_BUCKET=gke-pv-backup-storage
+SOURCE_CLOUD_RUN_JOB_NAME="bfg-backup-job-${REGION}"
+SOURCE_CLOUD_SCHEDULER_JOB_NAME="bfg-backup-scheduler-${REGION}"
+DR_CLOUD_RUN_JOB_NAME="bfg-backup-job-${DR_REGION}"
+DR_CLOUD_SCHEDULER_JOB_NAME="bfg-backup-scheduler-${DR_REGION}"
+
+SERVICE_ACCOUNT="${PROJECT_NUMBER}-compute@developer.gserviceaccount.com"
 
 gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-  --member="serviceAccount:${PROJECT_NUMBER}-compute@developer.gserviceaccount.com" \
+  --member="serviceAccount:$SERVICE_ACCOUNT" \
   --role="roles/container.admin"
 
 gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-  --member="serviceAccount:${PROJECT_NUMBER}-compute@developer.gserviceaccount.com" \
+  --member="serviceAccount:$SERVICE_ACCOUNT" \
   --role="roles/compute.viewer"
 
 gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-  --member="serviceAccount:${PROJECT_NUMBER}-compute@developer.gserviceaccount.com" \
+  --member="serviceAccount:$SERVICE_ACCOUNT" \
   --role="roles/gkebackup.backupAdmin"
 
 gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-  --member="serviceAccount:${PROJECT_NUMBER}-compute@developer.gserviceaccount.com" \
+  --member="serviceAccount:$SERVICE_ACCOUNT" \
   --role="roles/storage.objectCreator"
 
 gcloud storage buckets add-iam-policy-binding "gs://${PV_STORAGE_BUCKET}" \
-  --member="serviceAccount:${PROJECT_NUMBER}-compute@developer.gserviceaccount.com" \
+  --member="serviceAccount:$SERVICE_ACCOUNT" \
   --role="roles/storage.objectAdmin"
 
-gcloud run jobs create "$CLOUD_RUN_JOB_NAME" \
+# Create the job to backup the SOURCE_CLUSTER
+gcloud run jobs create "$SOURCE_CLOUD_RUN_JOB_NAME" \
   --image "us-docker.pkg.dev/$PROJECT_ID/$DOCKER_REPO_NAME/scheduled-backup:v1" \
-  --region "$DR_REGION" \
+  --region "$REGION" \
   --task-timeout=5m \
   --max-retries=3 \
-  --service-account="${PROJECT_NUMBER}-compute@developer.gserviceaccount.com"
-
-gcloud run jobs add-iam-policy-binding "$CLOUD_RUN_JOB_NAME" \
-  --region="$DR_REGION" \
-  --member="serviceAccount:${PROJECT_NUMBER}-compute@developer.gserviceaccount.com" \
-  --role="roles/run.invoker"
-
-gcloud run jobs create "$CLOUD_RUN_JOB_NAME" \
-  --image "us-docker.pkg.dev/$PROJECT_ID/$DOCKER_REPO_NAME/scheduled-backup:v1" \
-  --region "$DR_REGION" \
-  --task-timeout=5m \
-  --max-retries=3 \
-  --service-account="${PROJECT_NUMBER}-compute@developer.gserviceaccount.com" \
+  --service-account="$SERVICE_ACCOUNT" \
   --set-env-vars="PROJECT_ID=$PROJECT_ID,\
 SOURCE_CLUSTER=$SOURCE_CLUSTER,\
 REGION=$REGION,\
@@ -58,3 +43,57 @@ NAMESPACE=$NAMESPACE,\
 BACKUP_NAME=$BACKUP_NAME,\
 BACKUP_PLAN_NAME=$BACKUP_PLAN_NAME,\
 PV_STORAGE_BUCKET=$PV_STORAGE_BUCKET"
+
+gcloud run jobs add-iam-policy-binding "$SOURCE_CLOUD_RUN_JOB_NAME" \
+  --region="$REGION" \
+  --member="serviceAccount:$SERVICE_ACCOUNT" \
+  --role="roles/run.invoker"
+
+# Create the job to backup the TARGET_CLUSTER
+gcloud run jobs create "$DR_CLOUD_RUN_JOB_NAME" \
+  --image "us-docker.pkg.dev/$PROJECT_ID/$DOCKER_REPO_NAME/scheduled-backup:v1" \
+  --region "$DR_REGION" \
+  --task-timeout=5m \
+  --max-retries=3 \
+  --service-account="$SERVICE_ACCOUNT" \
+  --set-env-vars="PROJECT_ID=$PROJECT_ID,\
+SOURCE_CLUSTER=$TARGET_CLUSTER,\
+REGION=$DR_REGION,\
+DR_REGION=$REGION,\
+NAMESPACE=$NAMESPACE,\
+BACKUP_NAME=$BACKUP_NAME,\
+BACKUP_PLAN_NAME=$BACKUP_PLAN_NAME,\
+PV_STORAGE_BUCKET=$PV_STORAGE_BUCKET"
+
+gcloud run jobs add-iam-policy-binding "$DR_CLOUD_RUN_JOB_NAME" \
+  --region="$DR_REGION" \
+  --member="serviceAccount:$SERVICE_ACCOUNT" \
+  --role="roles/run.invoker"
+
+gcloud scheduler jobs create http $SOURCE_CLOUD_SCHEDULER_JOB_NAME \
+  --location=us-west1 \
+  --schedule='*/10 * * * *' \
+  --uri="https://run.googleapis.com/v2/projects/$PROJECT_ID/locations/${REGION}/jobs/${SOURCE_CLOUD_RUN_JOB_NAME}:run" \
+  --http-method=POST \
+  --oauth-service-account-email="$SERVICE_ACCOUNT" \
+  --oauth-token-scope='https://www.googleapis.com/auth/cloud-platform' \
+  --description='Triggers Cloud Run Job bfg-backup-job every 10 minutes for backup.' \
+  --headers='User-Agent=Google-Cloud-Scheduler' \
+  --attempt-deadline='180s' \
+  --min-backoff='5s' \
+  --max-backoff='3600s' \
+  --max-doublings=5
+
+gcloud scheduler jobs create http $DR_CLOUD_SCHEDULER_JOB_NAME \
+  --location=us-west1 \
+  --schedule='*/10 * * * *' \
+  --uri="https://run.googleapis.com/v2/projects/$PROJECT_ID/locations/${DR_REGION}/jobs/${DR_CLOUD_RUN_JOB_NAME}:run" \
+  --http-method=POST \
+  --oauth-service-account-email="$SERVICE_ACCOUNT" \
+  --oauth-token-scope='https://www.googleapis.com/auth/cloud-platform' \
+  --description='Triggers Cloud Run Job bfg-backup-job every 10 minutes for backup.' \
+  --headers='User-Agent=Google-Cloud-Scheduler' \
+  --attempt-deadline='180s' \
+  --min-backoff='5s' \
+  --max-backoff='3600s' \
+  --max-doublings=5
