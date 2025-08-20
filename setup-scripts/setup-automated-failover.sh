d0079af0a47ed15024949a421e812c1e7ff16676
Will Sulzer <willsulzer@google.com>
2025-08-20 15:27:36 +0000

    automated failover from dr to source cluster

--- setup-scripts/setup-automated-failover.sh
+++ setup-scripts/setup-automated-failover.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -eux
 
+# Automated failure for Source to DR region
 SOURCE_ALERT_TOPIC_NAME="alert-topic-gke-apiserver-$REGION"
 
 gcloud pubsub topics create $SOURCE_ALERT_TOPIC_NAME
 
-NOTIFICATION_CHANNEL=$(gcloud alpha monitoring channels create \
+SOURCE_NOTIFICATION_CHANNEL=$(gcloud alpha monitoring channels create \
   --display-name="GKE Alert Pub/Sub $SOURCE_CLUSTER" \
   --type=pubsub \
   --channel-labels=topic="projects/$PROJECT_ID/topics/$SOURCE_ALERT_TOPIC_NAME" \
@@ -18,17 +19,48 @@ gcloud pubsub topics add-iam-policy-binding $SOURCE_ALERT_TOPIC_NAME \
 # Setup Template vars 
 export TPL_CLUSTER_NAME=$SOURCE_CLUSTER
 export TPL_PROJECT_ID=$PROJECT_ID
-export TPL_NOTIFICATION_CHANNEL=$NOTIFICATION_CHANNEL
+export TPL_NOTIFICATION_CHANNEL=$SOURCE_NOTIFICATION_CHANNEL
 envsubst < gke-control-plane-unreachable.json.tpl > gke-control-plane-unreachable-${SOURCE_CLUSTER}.json
 
 gcloud alpha monitoring policies create \
   --policy-from-file=gke-control-plane-unreachable-${SOURCE_CLUSTER}.json
 
 gcloud eventarc triggers create failover-trigger-$REGION \
-    --destination-run-service="$JOB_INVOKER_SERVICE_NAME" \
+    --destination-run-service="$JOB_INVOKER_SERVICE_NAME-$DR_REGION" \
     --destination-run-region="$DR_REGION" \
     --event-filters="type=google.cloud.pubsub.topic.v1.messagePublished" \
     --transport-topic="projects/$PROJECT_ID/topics/$SOURCE_ALERT_TOPIC_NAME" \
     --location="us-central1" \
     --service-account="$SERVICE_ACCOUNT"
 
+# Automated failure for DR to Source region
+DR_ALERT_TOPIC_NAME="alert-topic-gke-apiserver-$DR_REGION"
+
+gcloud pubsub topics create $DR_ALERT_TOPIC_NAME
+
+DR_NOTIFICATION_CHANNEL=$(gcloud alpha monitoring channels create \
+  --display-name="GKE Alert Pub/Sub $TARGET_CLUSTER" \
+  --type=pubsub \
+  --channel-labels=topic="projects/$PROJECT_ID/topics/$DR_ALERT_TOPIC_NAME" \
+  --format="value(name)")
+
+gcloud pubsub topics add-iam-policy-binding $DR_ALERT_TOPIC_NAME \
+    --member="serviceAccount:service-$PROJECT_NUMBER@gcp-sa-monitoring-notification.iam.gserviceaccount.com" \
+    --role="roles/pubsub.publisher"
+
+# Setup Template vars 
+export TPL_CLUSTER_NAME=$TARGET_CLUSTER
+export TPL_PROJECT_ID=$PROJECT_ID
+export TPL_NOTIFICATION_CHANNEL=$DR_NOTIFICATION_CHANNEL
+envsubst < gke-control-plane-unreachable.json.tpl > gke-control-plane-unreachable-${TARGET_CLUSTER}.json
+
+gcloud alpha monitoring policies create \
+  --policy-from-file=gke-control-plane-unreachable-${TARGET_CLUSTER}.json
+
+gcloud eventarc triggers create failover-trigger-$DR_REGION \
+    --destination-run-service="$JOB_INVOKER_SERVICE_NAME-$REGION" \
+    --destination-run-region="$REGION" \
+    --event-filters="type=google.cloud.pubsub.topic.v1.messagePublished" \
+    --transport-topic="projects/$PROJECT_ID/topics/$DR_ALERT_TOPIC_NAME" \
+    --location="us-central1" \
+    --service-account="$SERVICE_ACCOUNT"
--- setup-scripts/setup-cloud-run-job-invoker.sh
+++ setup-scripts/setup-cloud-run-job-invoker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 
-gcloud run deploy $JOB_INVOKER_SERVICE_NAME \
+gcloud run deploy $JOB_INVOKER_SERVICE_NAME-$DR_REGION \
   --image "us-docker.pkg.dev/$PROJECT_ID/$DOCKER_REPO_NAME/job-invoker:v1" \
   --region $DR_REGION \
   --service-account $SERVICE_ACCOUNT \
@@ -9,12 +9,30 @@ gcloud run deploy $JOB_INVOKER_SERVICE_NAME \
   --no-allow-unauthenticated \
   --set-env-vars="PROJECT_ID=$PROJECT_ID,JOB_REGION=$DR_REGION,JOB_NAME=$SOURCE_TO_DR_FAILOVER_CLOUD_RUN_JOB_NAME"
 
-gcloud run services add-iam-policy-binding $JOB_INVOKER_SERVICE_NAME \
+gcloud run services add-iam-policy-binding $JOB_INVOKER_SERVICE_NAME-$DR_REGION \
   --region=$DR_REGION \
   --member="serviceAccount:$SERVICE_ACCOUNT" \
   --role="roles/run.invoker"
 
 gcloud run jobs add-iam-policy-binding $SOURCE_TO_DR_FAILOVER_CLOUD_RUN_JOB_NAME \
   --region=$DR_REGION \
+  --member="serviceAccount:$SERVICE_ACCOUNT" \
+  --role="roles/run.invoker"
+
+gcloud run deploy $JOB_INVOKER_SERVICE_NAME-$REGION \
+  --image "us-docker.pkg.dev/$PROJECT_ID/$DOCKER_REPO_NAME/job-invoker:v1" \
+  --region $REGION \
+  --service-account $SERVICE_ACCOUNT \
+  --ingress internal \
+  --no-allow-unauthenticated \
+  --set-env-vars="PROJECT_ID=$PROJECT_ID,JOB_REGION=$REGION,JOB_NAME=$DR_TO_SOURCE_FAILOVER_CLOUD_RUN_JOB_NAME"
+
+gcloud run services add-iam-policy-binding $JOB_INVOKER_SERVICE_NAME-$REGION \
+  --region=$REGION \
+  --member="serviceAccount:$SERVICE_ACCOUNT" \
+  --role="roles/run.invoker"
+
+gcloud run jobs add-iam-policy-binding $DR_TO_SOURCE_FAILOVER_CLOUD_RUN_JOB_NAME \
+  --region=$REGION \
   --member="serviceAccount:$SERVICE_ACCOUNT" \
   --role="roles/run.invoker"
